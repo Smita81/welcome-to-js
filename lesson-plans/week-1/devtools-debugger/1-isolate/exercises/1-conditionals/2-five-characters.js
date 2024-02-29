fd8d0fb1e7bb3086550146a768274d76ff02923a
smita81 <smitabbsaraswat@gmail.com>
2024-02-29 19:45:36 +0100

    fiveCharacters

--- lesson-plans/week-1/devtools-debugger/1-isolate/exercises/1-conditionals/2-five-characters.js
+++ lesson-plans/week-1/devtools-debugger/1-isolate/exercises/1-conditionals/2-five-characters.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let input = prompt('enter something with 5 characters:');
+/*let input = prompt('enter something with 5 characters:');
 
 let message = '';
 if (input === null) {
@@ -20,3 +20,20 @@ alert(message);
   - Change input's value after asking for the user input so that we always show "thank you!".
   - Add one more conditional so that we never show ":(" and don't always show "thank you!".
 */
+
+let Input = Number;
+Input = prompt('enter something with 4 numbers:');
+let info = '';
+
+// info = 'thank you'; // always show thank you
+if (Input.length > 4) {
+  info = 'too long';
+} else if (Input.length < 4) {
+  info = 'too short';
+} else if (Input.length === 4) {
+  info = 'thank you!';
+} else {
+  info = ':('; // this will never executed
+}
+
+alert(info);
