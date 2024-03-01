8799ad5d0333a2304d3f2247257e444b212bacea
smita81 <smitabbsaraswat@gmail.com>
2024-03-01 17:10:05 +0100

    whileLoopsFiveCharacters

--- lesson-plans/week-1/devtools-debugger/1-isolate/exercises/2-while-loops/2-five-characters.js
+++ lesson-plans/week-1/devtools-debugger/1-isolate/exercises/2-while-loops/2-five-characters.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let input = null;
+/*let input = null;
 while (input === null || input.length !== 5) {
   input = prompt('enter something with 5 characters:');
 }
@@ -13,3 +13,17 @@ alert(message);
   - Change the condition to 10 characters.
   - Make the user write "everything" in 2 steps.
 */
+
+
+let Input;
+
+while(!Input || Input.length !== 10) {
+  let A = prompt('enter something with 10 characters:');
+  let B = prompt('enter something with 10 characters:');
+
+  if (A && B) {
+    Input = A + B;
+  } else {
+    alert ('you need to enter something');
+  }
+}
